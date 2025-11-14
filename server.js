66eada1e9407467ed61acf89b5d02f3b5c3b46fc
nks\ranchana <ranchana@netkasystem.com>
2025-11-14 14:54:22 +0700

    remove public folder

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from root directory
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
   console.log('Server running http://localhost:' + PORT);
